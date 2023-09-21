e889fd8872aa2cfc957d35e4714712afc1aba5cb
unknown <sounakray18@gmail.com>
2023-09-21 15:58:42 +0530

    encapsulation ass

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public RWOnly(String name) {
+        this.name = name;
+    }
+
+    public RWOnly() {
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
